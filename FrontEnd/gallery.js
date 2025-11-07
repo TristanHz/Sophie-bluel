5ab9055b3ff9710992a62590115cb3b57001cb06
Tristan <Hernandez34300@gmail.com>
2025-11-07 14:51:54 +0100

    Ajout bouton filtre et début fonctionnement

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -22,3 +22,44 @@ works.forEach(work => {
 })}
 
 getworks();
+
+async function getCat() {
+    const reponse =await fetch("http://localhost:5678/api/categories");
+    const categories =await reponse.json();
+    categories.unshift({"id":0, "name":"Tous"});
+
+    const boutonFiltrer = document.getElementById("filtres")
+
+    categories.forEach(categorie => {
+
+        const boutons = document.createElement("button");
+
+        boutons.innerText = categorie.name;
+        boutons.id = categorie.id;
+        boutons.className = "boutonfiltre";
+        boutonFiltrer.appendChild(boutons);
+
+        const button = document.querySelector('button')
+        const works = document.querySelectorAll("figure")
+        const id = button.id
+        const workId = works.id
+
+        boutons.addEventListener("click", () => {
+
+            works.forEach(work => {
+                if (workId === 0) {
+                    work.style.display = "";
+                } else if (workId != id) {
+                    work.style.display = "none";
+                } else {
+                    work.style.display = "";
+                }})
+
+        
+
+    })      
+    })}
+
+getCat();
+
+
